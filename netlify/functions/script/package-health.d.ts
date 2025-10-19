bb65cf4abfa366923bfb957c03b878761e480fb3
seunfola <seunfola1@gmail.com>
2025-10-19 21:32:16 +0100

    ✅ fix: recursive copy dist/src/*

--- netlify/functions/script/package-health.d.ts
+++
@@ -1,3 +0,0 @@
-import { Model } from 'mongoose';
-import { RepoHealthDocument } from '../src/repo-health/repo-health/repo-health.model';
-export declare const createMockModel: () => Model<RepoHealthDocument>;
